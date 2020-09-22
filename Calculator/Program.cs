158092d0fc1dbd979d543ec84c398e6a985ab375
HENDRIK <hendrik@KALI.home>
2020-09-22 19:07:36 +0200

    calculator works in rider, can't compile to exe yet due to namespace

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Calculator.calc;
 
 namespace Calculator
 {
@@ -6,7 +7,18 @@ namespace Calculator
     {
         private static void Main(string[] args)
         {
-            
+            Console.WriteLine("What kind of calculation do you want to do? +,-,*,/");
+            var outputOne = Console.ReadLine();
+            var opp = outputOne;
+            Console.WriteLine("What is the first number?");
+            var outputTwo = Console.ReadLine();
+            var numberOne = int.Parse(outputTwo);
+            Console.WriteLine("What is the second number?");
+            var outputThree = Console.ReadLine();
+            var numberTwo = int.Parse(outputThree);
+            var output = calc.Calculator.Calculate(opp, numberOne, numberTwo);
+            Console.WriteLine("The result is "+ output);
+
         }
     }
 }
--- Calculator/calc/Calculator.cs
+++ Calculator/calc/Calculator.cs
@@ -1,7 +1,32 @@
+using System;
+
 namespace Calculator.calc
 {
-    public class Calculator
+    public static class Calculator
     {
-        
+         public static int Calculate(string opp, int num1, int num2)
+        { 
+            var result = 0;
+            switch (opp)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Something went wrong...");
+                    break;
+            }
+
+            return result;
+        }
     }
 }
